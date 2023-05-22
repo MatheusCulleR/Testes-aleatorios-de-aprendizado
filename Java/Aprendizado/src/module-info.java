178de14bbdd1bd403eda9c25f0b244f6f95acc13
Matheus Ferreira <m.ferreira.g.freitas@gmail.com>
2023-05-22 10:56:00 -0300

    commit inicial

---
+++ Java/Aprendizado/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mattc
+ *
+ */
+module Aprendizado {
+}
---
+++ ProjetoAppYugi.py
@@ -0,0 +1,10 @@
+#um mapa, onde coloca a regiao onde você mora, e aparece outros jogadores no mapa ao redor, onde você convida ela a jogar fisico
+'''
+inserir a região, cidade ou local proximo
+criar um banco de dados onde apareceria cada pessoa para convidar pessoas para jogar
+pessoa mais proxima apareceria como se fosse "tinder"
+existe jogos ranqueados e casuais
+ranking para subir dentro do aplicativo
+casuais não mostram os decks, enquanto rankeados mostram os 3 decks mais utilizados dos jogadores
+necessario ao minimo 10 jogos casuais para iniciar a ranked
+'''
